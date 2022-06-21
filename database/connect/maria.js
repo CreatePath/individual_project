3916ba1658f7202084d43e4a25d6af5b9382329c
Suhyun Lee <hary0427@naver.com>
2022-06-21 15:17:47 +0900

    DB 변경

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -1,9 +1,16 @@
 const maria = require('mysql');
 
 const conn = maria.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'dltngus1',
+    //localhost 사용시
+    // host:'localhost',
+    // user:'root',
+    // password:'dltngus1',
+    // database:'clubs',
+    //dateStrings: true
+    // heroku 사용시
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "bc8f4f8b6e4f2f",
+    password: "81241a6c",
     database:'clubs',
     dateStrings: true
 })
