b27d38ffb07282d414964f23c44311201bb92ee6
CreatePath <hary0427@naver.com>
2022-07-20 15:22:40 +0900

    생각해보니 필요없는거 제거

--- database/connect/maria.js
+++ database/connect/maria.js
@@ -4,13 +4,6 @@ require('dotenv').config('.env');
 const dbconfig = {
     connectionLimit: 20,
 
-    // localhost 사용시
-    // host:'localhost',
-    // user:'root',
-    // password:'dltngus1',
-    // database:'clubs'
-
-    // heroku 사용시
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
